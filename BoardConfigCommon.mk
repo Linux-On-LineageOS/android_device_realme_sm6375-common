743a54614203089aa3eaf96b1cd836aad08d9478
Archie Meng <archiemeng@protonmail.com>
2025-11-03 19:48:02 +0800

    Disable SELinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -211,3 +211,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit the proprietary files
 include vendor/realme/sm6375-common/BoardConfigVendor.mk
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
